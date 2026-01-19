46791efe7a2fa76a724be83f0c72f7cfc2082bb0
Shoccio <mynameismrj019@gmail.com>
2026-01-19 10:36:41 +0800

    Took off cookie checking for student_courses routes

--- backend/routes/student_courses_routes.py
+++ backend/routes/student_courses_routes.py
@@ -1,7 +1,5 @@
-from fastapi import APIRouter, Depends, UploadFile, File
+from fastapi import APIRouter, UploadFile, File
 from functions.student_course_func import updateGrades, getStudentCourses, updateGradesBulk
-from functions.auth_func import checkRole
-from sqlalchemy.orm import Session
 from pydantic import BaseModel
 import csv
 import io
@@ -13,15 +11,15 @@ class Grades(BaseModel):
     remark: str 
 
 @router.get("/get")
-def getStudentCourse(student_id: str, program_id: str, role: str = checkRole(["admin", "student"])):
+def getStudentCourse(student_id: str, program_id: str):
     return getStudentCourses(student_id, program_id)
 
 @router.patch("/update-grade/{student_id}-{course_id}")
-def updateGrade(course_id: str, student_id: str, newGrades: Grades, role: str = checkRole(["admin"])):
+def updateGrade(course_id: str, student_id: str, newGrades: Grades):
     return updateGrades(course_id, student_id, newGrades.grade, newGrades.remark)
 
 @router.post("/update-grades-bulk/{student_id}")
-async def updateGradesBulkRoute(student_id: str, file: UploadFile = File(...), role: str = checkRole(["admin"])):
+async def updateGradesBulkRoute(student_id: str, file: UploadFile = File(...)):
     contents = await file.read()
     csv_data = csv.DictReader(io.StringIO(contents.decode("utf-8")))
     

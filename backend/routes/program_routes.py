16962049baf79a0100ced077d45dc1264410189b
Shoccio <mynameismrj019@gmail.com>
2026-01-19 10:26:21 +0800

    Took off cookie checking for the program_routes

--- backend/routes/program_routes.py
+++ backend/routes/program_routes.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
 from functions.program_func import  getProgramFirestore
-from functions.auth_func import checkRole
 
 router = APIRouter()
 
 @router.get("/get")
-def getPrograms(role: str = checkRole(["admin", "student"])):
+def getPrograms():
     return getProgramFirestore()

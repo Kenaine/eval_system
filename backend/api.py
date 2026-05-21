b1822369b3f9e926344206a4a00472ebaeff9ace
Shoccio <mynameismrj019@gmail.com>
2026-05-21 08:24:14 +0800

    took off a slash in the last url

--- backend/api.py
+++ backend/api.py
@@ -15,7 +15,7 @@ origins = [
     "https://eval-system-eugz.onrender.com",
     "https://eval-system-ar1e.onrender.com",
     "https://evalsystem-production.up.railway.app",
-    "https://evaluation.uphsl.edu.ph/",
+    "https://evaluation.uphsl.edu.ph",
 ]
 
 app = FastAPI()
